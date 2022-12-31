aaee784ca9400761f6659dda9cbe201703e9c66e
kk2211 <kohlikabir101@gmail.com>
2023-01-01 00:19:57 +0530

    reading blob with cat command

--- app/main.py
+++ app/main.py
@@ -1,20 +1,38 @@
 import sys
 import os
+import zlib
+
+GIT_ROOT = ".git/"
+GIT_OBJECTS = ".git/objects/"
+GIT_REFS = ".git/refs/"
+GIT_HEAD = ".git/HEAD"
+
+def initialize():
+    os.mkdir(GIT_ROOT)
+    os.mkdir(GIT_OBJECTS)
+    os.mkdir(GIT_REFS)
+    with open(GIT_HEAD, "w") as f:
+            f.write("ref: refs/heads/master\n")
+
+
+def catCommand(hash):
+    dir = hash[:2]
+    fileName = hash[2:]
+    with open(f"{GIT_OBJECTS}/{dir}/{fileName}","rb") as f:
+        data = f.read()
+        data = zlib.decompress(data)
+        data = data.split(b"\x00",maxsplit=1)[1]
+        print(data.decode(), end="")
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    
     command = sys.argv[1]
     if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/master\n")
+        initialize()
         print("Initialized git directory")
+    elif command == "cat-file" and len(sys.argv) == 4:
+        catCommand(sys.argv[3])
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
